f4d6f68f4e82c739696d3f686255c0467a7c0312
Christian Schou <christian.schou@hotmail.com>
2024-12-28 14:05:57 +0100

    ✨ Global usings for application layer in framework

---
+++ src/Framework/LodgiQ.Framework.Application/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives for application layer in LodgiQ framework
+
+global using LodgiQ.Framework.Domain.Errors;
+global using LodgiQ.Framework.Domain.Results;
+global using MediatR;
