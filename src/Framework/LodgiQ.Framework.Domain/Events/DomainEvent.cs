7cbd09bec3a837d50ec5122084274f8a2e98340a
Christian Schou <christian.schou@hotmail.com>
2024-12-28 13:44:16 +0100

    ✨ Add Domain Events for ddd

---
+++ src/Framework/LodgiQ.Framework.Domain/Events/DomainEvent.cs
@@ -0,0 +1,19 @@
+namespace LodgiQ.Framework.Domain.Events;
+
+public abstract class DomainEvent : IDomainEvent
+{
+    protected DomainEvent()
+    {
+        Id = Guid.NewGuid();
+        HappenedAtUtc = DateTime.UtcNow;
+    }
+
+    protected DomainEvent(Guid id, DateTime happenedAtUtc)
+    {
+        Id = id;
+        HappenedAtUtc = happenedAtUtc;
+    }
+    
+    public Guid Id { get; }
+    public DateTime HappenedAtUtc { get; }
+}
---
+++ src/Framework/LodgiQ.Framework.Domain/Events/IDomainEvent.cs
@@ -0,0 +1,8 @@
+namespace LodgiQ.Framework.Domain.Events;
+
+public interface IDomainEvent
+{
+    Guid Id { get; }
+    
+    DateTime HappenedAtUtc { get; }
+}
