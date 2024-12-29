67ca9a29e822e7ff6cca6ed971f646e2202a08c4
Christian Schou <christian.schou@hotmail.com>
2024-12-29 11:23:03 +0100

    👔 Add configuration extensions
    These extensions can help retrieve a connection string and a specific value from a specific key when loaded into the configuration at startup.

---
+++ src/Framework/LodgiQ.Framework.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+
+namespace LodgiQ.Framework.Infrastructure.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static string GetConnectionStringOrThrow(this IConfiguration configuration, string name)
+    {
+        return configuration.GetConnectionString(name) ??
+               throw new InvalidOperationException($"The connection string {name} was not found");
+    }
+
+    public static TValue GetValueOrThrow<TValue>(this IConfiguration configuration, string name)
+    {
+        return configuration.GetValue<TValue?>(name) ??
+               throw new InvalidOperationException($"The value for: {name} was not found");
+    }
+}
